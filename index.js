bc379706b84b0cb89bfb166ae761cbcadd92cb06
jaudir <5773247+jaudir@users.noreply.github.com>
2020-01-27 15:59:18 -0300

    feat: Implementa os middlewares

--- index.js
+++ index.js
@@ -15,25 +15,52 @@ function logReqs(req, res, next) {
 server.use(logReqs);
 
 function checkIdProject(req, res, next) {
+  const { id } = req.params;
+  const p = projects.find(i => i.id == id);
+
+  if (!p) {
+    return res.status(400).json({ error: "Project not found!" });
+  }
+
   return next();
 }
 
 server.post("/projects", (req, res) => {
-  return;
+  const { id, title } = req.body;
+
+  p = {
+    id,
+    title,
+    task: []
+  };
+
+  projects.push(p);
+  return res.json(p);
 });
 
 server.get("/projects", (req, res) => {
-  return;
+  return res.json(projects);
 });
 
 server.put("/projects/:id", checkIdProject, (req, res) => {
-  return;
+  const { id } = req.params;
+  const { title } = req.body;
+  const p = projects.find(i => i.id == id);
+  p.title = title;
+  return res.json(p);
 });
 
 server.delete("/projects/:id", checkIdProject, (req, res) => {
-  return;
+  const { id } = req.params;
+  const index = projects.findIndex(i => i.id == id);
+  projects.splice(index, 1);
+  return res.send();
 });
 
-server.post("/projects/:id/tasks", (req, res) => {
-  return;
+server.post("/projects/:id/tasks", checkIdProject, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  const p = projects.find(i => i.id == id);
+  p.task.push(title);
+  return res.json(p);
 });
